1f6921e0cfe1abc35d23088db6a0536af8bac92b
Habibur Rahman <114073375+mefury@users.noreply.github.com>
2025-09-23 23:54:05 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@
 # This script downloads the nexus-network executable to the current directory and makes it executable.
 
 # Define environment variables
-DOWNLOAD_URL="https://raw.githubusercontent.com/mefury/unlocked/750ea8ad0f74f739d69c599ffc465519e2426821/nexus-network"
+DOWNLOAD_URL="https://raw.githubusercontent.com/mefury/unlocked/59a2a389a57e781f91cda36fd75bfc3e1d547c49/nexus-network"
 
 # Check if the executable already exists in the current directory
 EXECUTABLE="./nexus-network"
